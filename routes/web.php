b4d52e0730ff4ded22a4c366f0a0e1b30b87f96d
Huyghues des Etages Thomas <10020@odhlm93.pri>
2019-06-05 17:04:09 +0200

    divers tests

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $view = view('welcome');
+    $view->first_name = 'The';
+    $view->last_name = 'Screamer';
+    return $view;
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/help', function () {
+    return view('pages.help');
+});
+
+Route::get('/events', function () {
+
+    return view('pages.events', [
+        'Event_1' => 'premier event',
+        'Event_2' => 'deuxieme event',
+        'Event_3' => 'troisieme event',
+    ]);
 });
